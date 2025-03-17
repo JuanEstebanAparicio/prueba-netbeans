21d9f727c1a7e2559436040ac24c970c75579e6b
JuanEstebanAparicio <apariciojuanesteban@gmail.com>
2025-03-16 20:05:22 -0500

    mi primer commit

---
+++ src/primerrepositorio_netbeans/PrimerRepositorio_Netbeans.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package primerrepositorio_netbeans;
+
+/**
+ *
+ * @author juana
+ */
+public class PrimerRepositorio_Netbeans {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
